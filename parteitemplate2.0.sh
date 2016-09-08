2242fec71995478fd8f98c500f3090d59e906720
mpjanz <manuel.p.janz@googlemail.com>
2016-09-08 09:56:41 +0200

    Add files via upload

---
+++ parteitemplate2.0.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+  ordner=$(dirname $0)
+  cd $ordner
+  cp $ordner/daten $ordner/tempdaten~ 
+  kreisverband='Leverkusen'
+  homepage='www.diePARTEI.net/Leverkusen'
+  echo 'um ein Hintergrundbild zu benutzen jetzt eine PNG Datei mit dem Namen hintergrund.png im Verzeichnis des Scripts speichern'
+  echo Zeile 1 eingeben
+  read zeile1
+  echo Zeile 2 eingeben
+  read zeile2
+  echo Zeile 3 eingeben
+  read zeile3
+  echo Zeile 4 eingeben
+  read zeile4
+  echo untertitel eingeben
+  read untertitel
+  echo Kreisverband eingeben
+  read kreisverband
+  echo Internetadresse eingeben
+  read homepage
+  echo Dateinamen eingeben
+  read dateiname
+
+    
+
+  sed -i -e "s/Zeile1/$zeile1/g" $ordner/tempdaten~
+  sed -i -e "s/Zeile2/$zeile2/g" $ordner/tempdaten~
+  sed -i -e "s/Zeile3/$zeile3/g" $ordner/tempdaten~
+  sed -i -e "s/Zeile4/$zeile4/g" $ordner/tempdaten~
+  sed -i -e "s/untertitel/$untertitel/g" $ordner/tempdaten~
+  sed -i -e "s/kreisverband/$kreisverband/g" $ordner/tempdaten~
+  sed -i -e "s/internetadresse/$homepage/g" $ordner/tempdaten~
+
+
+  daten=$(<$ordner/tempdaten~)
+  echo $daten > $dateiname'.svg'
+  
+  rm $ordner/tempdaten~
+  
+  inkscape --export-png=$ordner/$dateiname --export-pdf=$ordner/$dateiname'.pdf' $ordner/$dateiname'.svg'
+
+  
+
